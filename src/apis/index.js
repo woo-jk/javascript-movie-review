f4121d99f631f4751d44eae1268e4b055f11fd1f
woo-jk <201702042@o.cnu.ac.kr>
2023-03-16 16:34:47 +0900

    fix: APIKEY 에러 수정

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import { popularUrl, searchUrl } from "../constants/urls";
 import fetchApi from "../utils/fetchApi";
 
 export const fetchMovieListWithKeyword = async (pageNumber, keyword) => {
-  const url = `${searchUrl}?api_key=${process.env.API_KEY}&query=${keyword}&language=ko&page=${pageNumber}`;
+  const url = `${searchUrl}?api_key=${process.env.WEBPACK_API_KEY}&query=${keyword}&language=ko&page=${pageNumber}`;
 
   return await fetchApi(url);
 };
